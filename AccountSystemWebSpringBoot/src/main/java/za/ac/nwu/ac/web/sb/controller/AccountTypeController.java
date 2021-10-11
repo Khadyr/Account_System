4ee3cdc70b539eebb51b85671cf51b4c213e7064
Khadyr <zanderjvr@live.co.za>
2021-10-11 06:28:27 +0200

    Added spring end to end

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -3,26 +3,37 @@ package za.ac.nwu.ac.web.sb.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
+import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("account-type")
 public class AccountTypeController {
-    @GetMapping("/ping")
-    @ApiOperation(value = "Echo the Ping.", notes = "This echo the ping back to the client")
+
+    private final FetchAccountTypeFlow fetchAccountTypeFlow;
+
+    @Autowired
+    public AccountTypeController(FetchAccountTypeFlow fetchAccountTypeFlow) {
+        this.fetchAccountTypeFlow = fetchAccountTypeFlow;
+    }
+
+    @GetMapping("/all")
+    @ApiOperation(value = "Gets all the configured Account types.", notes = "Returns a list of account types")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
+            @ApiResponse(code = 200, message = "Account types returned", response = GeneralResponse.class),
             @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
             @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
-    public ResponseEntity<String> ping(
-            @RequestParam(value = "value that will be echoed", defaultValue = "pong") String echo) {
-         return new ResponseEntity<>(echo, HttpStatus.OK);
+    public ResponseEntity<GeneralResponse<List<AccountTypeDto>>> getAll() {
+        List<AccountTypeDto> accountTypes = fetchAccountTypeFlow.getAllAccountTypes();
+        GeneralResponse<List<AccountTypeDto>> response = new GeneralResponse<>(true, accountTypes);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
